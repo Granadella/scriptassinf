76fb52cdf880f76e97f1d246303ad8e78cc0790b
Granadella <132225175+Granadella@users.noreply.github.com>
2025-01-25 18:17:54 -0300

    Update assinf.ps1

--- assinf.ps1
+++ assinf.ps1
@@ -394,7 +394,7 @@ function Atualizar-Script {
 }
 
 function Check-InvokeExpression {
-    if ($Host.Name -eq "ConsoleHost" -and $MyInvocation.PipelineLength -gt 0) {
+    if ($ExecutionContext.SessionState.PSCommandPath -match "iex|Invoke-RestMethod") {
         Write-Host "Aviso: O script foi chamado via irm e iex. A função de verificar atualizações não estará disponível." -BackgroundColor DarkRed
         return $true
     }
